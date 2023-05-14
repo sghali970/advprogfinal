2bf4657ec91852829eb0c314b44964ca0cd76bab
sghali970 <55302487+sghali970@users.noreply.github.com>
2023-05-14 11:33:50 -0700

    Made enemy (goblin) to move horizontally
    The goblin now moves horizontally back and forth. It moves left 55 pixels and then back right 55 pixels and continues this. I also made sure that collisions work (when the player collides with enemies).

--- finalprojrunner/goblin.gd
+++ finalprojrunner/goblin.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends Node2D
 
 
 const SPEED = 300.0
@@ -7,11 +7,12 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
 func _physics_process(delta):
+	$AnimatedSprite2D.flip_h = true
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -20,9 +21,9 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	velocity.x=direction*SPEED
 	if direction:
 		velocity.x = direction * SPEED
-	
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
---
+++ finalprojrunner/gobmove.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@export var move_speed : float = 55.0
+@export var move_direction : Vector2
+
+var start_position : Vector2
+var final_position : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.flip_h = true
+	start_position = global_position
+	final_position = start_position + move_direction
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_position = global_position.move_toward(final_position, move_speed*delta)
+	if global_position == final_position:
+		if global_position == start_position:
+			$AnimatedSprite2D.flip_h = true
+			final_position = start_position + move_direction
+		else:
+			$AnimatedSprite2D.flip_h = false
+			final_position = start_position
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		self.queue_free()
